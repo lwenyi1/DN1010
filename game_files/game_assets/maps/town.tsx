f80155ac81ebad26af778f34e6208b171b61f88d
Xavierleejrui <xavier.leesia@gmail.com>
2024-07-01 10:43:45 +0800

    Revert "Map"
    This reverts commit 4d7841969c7d691d3fd1ed0d316df26cbf6bc7e4.

--- game_files/game_assets/maps/town.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="town" tilewidth="32" tileheight="32" tilecount="99" columns="11">
- <image source="town_rpg_pack/town_rpg_pack/graphics/transparent-bg-tiles.png" width="352" height="288"/>
-</tileset>
--- game_files/game_assets/maps/transparent logic links.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Beach" tilewidth="16" tileheight="16" margin="1" tilecount="1131" columns="39">
- <image source="Logic Links/roguelikeCity_transparent.png" width="628" height="475"/>
-</tileset>
